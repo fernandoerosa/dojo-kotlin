c30dbe8e46dbc69b1950c8160ec1f441f6ff86fa
fernandoerosa <fernando.rosag11@gmail.com>
2023-09-20 00:47:01 +0000

    create alternative change calculate

--- src/main/kotlin/com/example/demo/DojoKotlinApplication.kt
+++ src/main/kotlin/com/example/demo/DojoKotlinApplication.kt
@@ -1,16 +1,60 @@
 package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication as run
 
-@SpringBootApplication
-class DojoKotlinApplication
+@SpringBootApplication class DojoKotlinApplication
 
 fun main(args: Array<String>) {
     val totalValue = 98.94 // Total amount to be paid
     val paidValue = 100.0 // Actual amount paid
 
+    alterantiveCalculateChange(totalValue, paidValue)
     calculateChange(totalValue, paidValue)
+}
+
+fun alterantiveCalculateChange(totalValue: Double, paidValue: Double) {
+
+    val change = paidValue - totalValue
+
+    if (change < 0) {
+        println("The paid amount is insufficient to cover the total due.")
+        return
+    }
+
+    val billsAndCoinsMap =
+            mapOf<Double, String>(
+                    100.0 to "100-real bills",
+                    50.0 to "50-real bills",
+                    10.0 to "10-real bills",
+                    5.0 to "5-real bills",
+                    1.0 to "1-real bills",
+                    0.5 to "50-cent coins",
+                    0.1 to "10-cent coins",
+                    0.05 to "5-cent coins",
+                    0.01 to "1-cent coins"
+            )
+
+    var remainingChange = change
+
+    var changeMap = mutableMapOf<Double, Int>()
+
+    println("Change to be provided:")
+
+    for ((i, value) in billsAndCoinsMap) {
+        val quantity = (remainingChange / i).toInt()
+        if (quantity > 0) {
+            changeMap[i] = quantity
+            remainingChange -= quantity * i
+        }
+    }
+
+    println("Map -> $changeMap")
+
+    for ((k, v) in changeMap) {
+        println("$v bills of R$ $k")
+    }
+
+    println("----------------")
 }
 
 fun calculateChange(totalValue: Double, paidValue: Double) {
@@ -29,7 +73,6 @@ fun calculateChange(totalValue: Double, paidValue: Double) {
 
     for (i in billsAndCoins.indices) {
         val quantity = (remainingChange / billsAndCoins[i]).toInt()
-        println(quantity)
         if (quantity > 0) {
             println("$quantity ${billAndCoinNames[i]}")
             remainingChange -= quantity * billsAndCoins[i]
